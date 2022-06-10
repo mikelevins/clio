c5166c726edad9ee24ae89c1cadb56a331a2d926
mikel evins <mevins@me.com>
2022-06-10 12:49:39 -0500

    tinkering with GUI app startup

--- clio/resources/js/main.js
+++ clio/resources/js/main.js
@@ -1,22 +1,9 @@
-// This is just a sample app. You can structure your Neutralinojs app code as you wish.
-// This example app is written with vanilla JavaScript and HTML.
-// Feel free to use any frontend framework you like :)
-// See more details: https://neutralino.js.org/docs/how-to/use-a-frontend-library
-
 function showInfo() {
     document.getElementById('info').innerHTML = `
         ${NL_APPID} is running on port ${NL_PORT}  inside ${NL_OS}
         <br/><br/>
         <span>server: v${NL_VERSION} . client: v${NL_CVERSION}</span>
         `;
-}
-
-function openDocs() {
-    Neutralino.os.open("https://neutralino.js.org/docs");
-}
-
-function openTutorial() {
-    Neutralino.os.open("https://www.youtube.com/watch?v=txDlNNsgSh8&list=PLvTbqpiPhQRb2xNQlwMs0uVV0IN8N-pKj");
 }
 
 function setTray() {
@@ -37,13 +24,13 @@ function setTray() {
 
 function onTrayMenuItemClicked(event) {
     switch(event.detail.id) {
-        case "VERSION":
-            Neutralino.os.showMessageBox("Version information",
-                `Neutralinojs server: v${NL_VERSION} | Neutralinojs client: v${NL_CVERSION}`);
-            break;
-        case "QUIT":
-            Neutralino.app.exit();
-            break;
+    case "VERSION":
+        Neutralino.os.showMessageBox("Version information",
+                                     `Neutralinojs server: v${NL_VERSION} | Neutralinojs client: v${NL_CVERSION}`);
+        break;
+    case "QUIT":
+        Neutralino.app.exit();
+        break;
     }
 }
 
