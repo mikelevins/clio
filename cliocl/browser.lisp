e97474297c22f833db56d1815f4ba81d4a80547a
mikel evins <mevins@me.com>
2022-06-11 13:00:06 -0500

    added code to open the webapp in chrome

--- cliocl/browser.lisp
+++ cliocl/browser.lisp
@@ -10,3 +10,13 @@
 
 (in-package :cliocl)
 
+(defun open-browser (&key (port 8000))
+  (let ((args (list (format nil "--app=http://localhost:~A" port))))
+    #+(or win32 mswindows windows)
+    (sb-ext:run-program "start chrome" args)
+    #+(or macos darwin)
+    (sb-ext:run-program "/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome" args)
+    #+linux
+    (sb-ext:run-program "/usr/bin/google-chrome" args)))
+
+#+nil (open-browser)
