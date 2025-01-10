8261237c8d8a4a82e9a0b4e9939682d3f292bd70
mikel evins <mevins@me.com>
2025-01-09 22:33:40 -0600

    more tinkering with command line args

--- stage/src/main.js
+++ stage/src/main.js
@@ -1,28 +1,19 @@
 const { invoke } = window.__TAURI__.core;
-const { getMatches } = window.__TAURI__.cli;
 
 let greetInputEl;
 let greetMsgEl;
 
-const cli_matches = await getMatches();
-
 async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-    greetMsgEl.textContent = await invoke("greet", { name: greetInputEl.value });
+  // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+  greetMsgEl.textContent = await invoke("greet", { name: greetInputEl.value });
 }
 
 window.addEventListener("DOMContentLoaded", () => {
     greetInputEl = document.querySelector("#greet-input");
     greetMsgEl = document.querySelector("#greet-msg");
-    climatchesEl = document.querySelector("#climatches-msg");
-    climatchesEl.textContent = JSON.stringify(cli_matches);
-    console.log("CLI matches:");
-    console.log(JSON.stringify(cli_matches));
-
+    
     document.querySelector("#greet-form").addEventListener("submit", (e) => {
         e.preventDefault();
         greet();
     });
 });
-
-// build: npm run tauri build -- --debug
