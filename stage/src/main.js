d3051b503f50537e61612c93723e1d7c8a01edff
mikel evins <mevins@me.com>
2025-01-10 08:06:11 -0600

    figured out how to get command-line args in JS

--- stage/src/main.js
+++ stage/src/main.js
@@ -1,16 +1,26 @@
 const { invoke } = window.__TAURI__.core;
+const { getMatches } = window.__TAURI__.cli;
 
 let greetInputEl;
 let greetMsgEl;
+let matchesMsgEl;
+let matchesData = "Matches";
 
 async function greet() {
-  // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-  greetMsgEl.textContent = await invoke("greet", { name: greetInputEl.value });
+    // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+    greetMsgEl.textContent = await invoke("greet", { name: greetInputEl.value });
+    matchesMsgEl.textContent = JSON.stringify(matchesData);
 }
+
+getMatches().then((result) => {
+    matchesData = result;
+    greet();
+});
 
 window.addEventListener("DOMContentLoaded", () => {
     greetInputEl = document.querySelector("#greet-input");
     greetMsgEl = document.querySelector("#greet-msg");
+    matchesMsgEl = document.querySelector("#matches-msg");
     
     document.querySelector("#greet-form").addEventListener("submit", (e) => {
         e.preventDefault();
