fea5f01035be3d7a820c7a8f06f0f85263084648
mikel evins <mikel@evins.net>
2025-02-07 11:36:53 -0600

    getting and testing CLI params in the UI

--- stage/src/main.js
+++ stage/src/main.js
@@ -3,8 +3,12 @@ const { getMatches } = window.__TAURI__.cli;
 
 let greetInputEl;
 let greetMsgEl;
-let matchesMsgEl;
-let matchesData = "Matches";
+let httpMsgEl;
+let httpData = "HTTP port: ";
+let wsMsgEl;
+let wsData = "WebSocket port: ";
+let swankMsgEl;
+let swankData = "Swank port: ";
 
 async function greet() {
     // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
@@ -12,14 +16,21 @@ async function greet() {
 }
 
 getMatches().then((result) => {
-    matchesData = result;
-    matchesMsgEl.textContent = JSON.stringify(matchesData);
+    let args = result.args;
+    let hp = args.http_port;
+    let wp = args.websocket_port;
+    let sp = args.swank_port;
+    httpMsgEl.textContent = httpData+hp.value;
+    wsMsgEl.textContent = wsData+wp.value;
+    swankMsgEl.textContent = swankData+sp.value;
 });
 
 window.addEventListener("DOMContentLoaded", () => {
     greetInputEl = document.querySelector("#greet-input");
     greetMsgEl = document.querySelector("#greet-msg");
-    matchesMsgEl = document.querySelector("#matches-msg");
+    httpMsgEl = document.querySelector("#http-msg");
+    wsMsgEl = document.querySelector("#ws-msg");
+    swankMsgEl = document.querySelector("#swank-msg");
     
     document.querySelector("#greet-form").addEventListener("submit", (e) => {
         e.preventDefault();
