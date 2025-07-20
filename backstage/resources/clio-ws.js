c72ea54ef6baade100cc92d6d1699811c620cccc
mikel evins <mevins@me.com>
2025-07-20 09:36:29 -0500

    working on lisp/browser protocols

--- backstage/resources/clio-ws.js
+++ backstage/resources/clio-ws.js
@@ -1,14 +1,36 @@
 var ClioSocket = new WebSocket("ws://127.0.0.1:40404/");
 
+// ---------------------------------------------------------------------
+// receiving messages
+// ---------------------------------------------------------------------
+
 ClioSocket.onmessage = function (event) {
     var data = JSON.parse(event.data);
-    console.log("event: ");
-    console.log(event);
-    console.log("data: ");
-    console.log(data);
+    //console.log(data);
+    handleWSEvent(data);
+}
+
+function handleWSEvent(eventData) {
+    let eventType = eventData['type'];
+    if (eventType == 'clear') {
+        Canvas.clear();
+    } else if (eventType == 'ping') {
+        console.log("Received WS ping");
+    } else{
+        console.log("Received WS event: ");
+        console.log(eventData);
+    }    
+}
+
+// ---------------------------------------------------------------------
+// sending messages
+// ---------------------------------------------------------------------
+
+function sendObject(object){
+    ClioSocket.send(JSON.stringify(object));
 }
 
 
 function pingLisp () {
-    ClioSocket.send("(:ping)");
+    sendObject("(:ping)");
 }
--- backstage/src/browser-api.lisp
+++ backstage/src/browser-api.lisp
@@ -10,9 +10,27 @@
 
 (in-package :backstage)
 
-(defun send-to-browser (msg-plist)
-  (let ((client (first (trivial-ws:clients backstage::*backstage-websocket-server*)))
-        (json (cl-json:encode-json-plist-to-string msg-plist)))
-    (trivial-ws:send client json)))
+;;; ---------------------------------------------------------------------
+;;; encode messages
+;;; ---------------------------------------------------------------------
 
-#+repl (send-to-browser '(:data "ping"))
+(defun encode-ping ()
+  (cl-json:encode-json-plist-to-string '(:type "ping")))
+
+#+repl (encode-ping)
+
+;;; ---------------------------------------------------------------------
+;;; send to the browser
+;;; ---------------------------------------------------------------------
+
+(defun send-to-browser (json-msg)
+  (let ((client (first (trivial-ws:clients backstage::*backstage-websocket-server*))))
+    (trivial-ws:send client json-msg)))
+
+#+repl (send-to-browser (encode-ping))
+
+
+#+repl (backstage::start-server backstage::*backstage-http-server-port*)
+#+repl (backstage::start-browser)
+#+repl (backstage::stop-server)
+#+repl (trivial-ws:clients backstage::*backstage-websocket-server*)
